995c33d0720e9b55aea4e55d101cc2f68a0a58e7
Luis Gomez <lgomezm90@gmail.com>
2020-11-19 18:01:09 -0500

    Updated load_data.py

--- load_data.py
+++ load_data.py
@@ -61,29 +61,44 @@ def create_invoice(customer_id):
 
 
 def main(argv):
-    n = 0
+    num_customers = 0
+    num_invoices = 0
+    num_charges = 0
     try:
-        opts, _ = getopt.getopt(argv, 'hn:')
+        opts, _ = getopt.getopt(argv, 'hn:i:c:')
     except getopt.GetoptError:
-        print('load_data.py -n <number-of-customers>')
+        print('load_data.py -n <number-of-customers> -i <invoices-per-customer> -c <charges-per-customer>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
             print('load_data.py -n <number-of-customers>')
             sys.exit()
         elif opt in ("-n") and arg.isnumeric():
-            n = int(arg)
-    if n <= 0:
-        print('Number of customers should be a positive integer')
+            num_customers = int(arg)
+        elif opt in ("-i") and arg.isnumeric():
+            num_invoices = int(arg)
+        elif opt in ("-c") and arg.isnumeric():
+            num_charges = int(arg)
+    if num_customers <= 0:
+        print('ERROR: Number of customers should be a positive integer')
+        sys.exit()
+    if num_invoices <= 0:
+        print('ERROR: Number of invoices should be a positive integer')
+        sys.exit()
+    if num_charges <= 0:
+        print('ERROR: Number of charges should be a positive integer')
+        sys.exit()
     seed(1)
-    for _ in range(0, n):
+    for _ in range(num_customers):
         try:
             customer_id = create_customer()
-            create_invoice(customer_id)
-            create_charge(customer_id)
-            logging.info('Created customer, charge and invoice. Customer id: %s', customer_id)
+            for _ in range(num_invoices):
+                create_invoice(customer_id)
+            for _ in range(num_charges):
+                create_charge(customer_id)
+            logging.info('Created customer, charges and invoices. Customer id: %s', customer_id)
         except:
-            logging.warning('Could not create data!')
+            logging.warning('ERROR: Could not create data. Skipping to next iteration!')
 
 
 if __name__ == "__main__":
